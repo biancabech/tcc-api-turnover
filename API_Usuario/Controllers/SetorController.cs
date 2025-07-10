5495157e4244007b448d9758dc4fc1aec10d1fa5
cauetrainotti <Maycoondrizz777@icloud.com>
2025-07-10 09:25:58 -0300

    Setor Finalizado

---
+++ API_Usuario/Controllers/SetorController.cs
@@ -0,0 +1,46 @@
+﻿using API_Usuario.DTOs;
+using API_Usuario.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Usuario.Controllers
+{
+
+    public class SetorController : ControllerBase
+    {
+        public readonly SetorService _services;
+
+        public SetorController(SetorService SetorServices)
+        {
+            _services = SetorServices;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            return Ok(await _services.GetAllSetor());
+        }
+        [HttpPost]
+        public async Task<ActionResult> Add([FromBody] SetorDTO dto)
+        {
+            string resultado = await _services.AddSetor(dto);
+            if (resultado.Contains("Não")) return BadRequest(resultado);
+            return Ok(resultado);
+
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Update(int id, [FromBody] SetorDTO dto)
+        {
+            string resultado = await _services.UpdateSetor(id, dto);
+            if (resultado.Contains("não encontrado")) return NotFound(resultado);
+            return Ok(resultado);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            string resultado = await _services.DeleteSetor(id);
+            if (resultado.Contains("não encontrado")) return NotFound(resultado);
+            return Ok(resultado);
+        }
+    }
+}
