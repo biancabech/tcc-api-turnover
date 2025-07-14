4de66368dae88bdb9aba13827d837f74a5c57e72
Baphomet <zloostpvp@gmail.com>
2025-07-13 21:42:22 -0300

    update funcionario

--- API_Usuario/Models/Funcionario.cs
+++ API_Usuario/Models/Funcionario.cs
@@ -7,17 +7,14 @@ namespace API_Usuario.Models
         public Guid Id { get; set; } = Guid.NewGuid();
 
         public string Nome { get; set; }
-
         public string Genero { get; set; }
-
+        public string Cpf { get; set; }
         public DateTime DataNasci { get; set; }
-
         public DateTime DataAdmi { get; set; }
-
         public DateTime? DataDemi { get; set; }
-
         public string Email { get; set; }
         public Guid CargoId { get; set; }
         public Cargo Cargo { get; set; }
+        public Setor Setor { get; set; }
     }
 }
