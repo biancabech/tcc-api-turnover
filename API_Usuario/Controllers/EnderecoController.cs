a62c96e859983c843e0d06d1dba97a1434cb5a9b
VitorMoraesFilho <vitordemoraesfilho@gmail.com>
2025-07-25 09:45:10 -0300

    add-Endereco

--- API_Usuario/Controllers/EnderecoController.cs
+++ API_Usuario/Controllers/EnderecoController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using API_Usuario.DTOs;
+using API_Usuario.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API_Usuario.Controllers
@@ -7,5 +9,39 @@ namespace API_Usuario.Controllers
     [ApiController]
     public class EnderecoController : ControllerBase
     {
+        public readonly EnderecoService _service;
+
+        public EnderecoController(EnderecoService Enderecoservice)
+        {
+            _service = Enderecoservice;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            return Ok(await _service.GetAllEndereco());
+        }
+        [HttpPost]
+        public async Task<ActionResult> Add([FromBody] EnderecoDTOs dto)
+        {
+            string resultado = await _service.AddEndereco(dto);
+            if (resultado.Contains("Não")) return BadRequest(resultado);
+            return Ok(resultado);
+        }
+        [HttpPut("{id}")]
+
+        public async Task<ActionResult> Update(Guid id, [FromBody] EnderecoDTOs dto){
+
+        string resultado = await _service.UpdateEndereco(id, dto);
+            if (resultado.Contains("Não encontrado")) return NotFound(resultado);
+            return Ok(resultado);
+    }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(Guid id)
+        {
+            string resultado = await _service.DeleteEndereco(id);
+            if (resultado.Contains("Não encontrado")) return NotFound(resultado);
+            return Ok(resultado);
+        }
     }
 }
