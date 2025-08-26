1483e95cb92463455829ec043083519a8954b62f
Bianca Bech <biancapaulabech@gmail.com>
2025-08-26 00:06:44 -0300

    Adicionado migration para alterar componente motivo de desligamento

---
+++ API_Usuario/Migrations/20250824155430_DeletarFuncionarioIdMotivoDesligamento.cs
@@ -0,0 +1,28 @@
+﻿using API_Usuario.Models;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+
+#nullable disable
+
+namespace API_Usuario.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeletarFuncionarioIdMotivoDesligamento : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey("FK_MotivoDesligamentos_Funcionarios_FuncionarioId", "motivodesligamentos");
+            migrationBuilder.DropForeignKey("FK_MotivoDesligamentos_Desligamentos_DesligamentoId", "motivodesligamentos");
+
+            migrationBuilder.DropColumn("FuncionarioId", "motivodesligamentos");
+            migrationBuilder.DropColumn("DesligamentoId", "motivodesligamentos");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            throw new Exception("Essa migration é irreversível");
+        }
+    }
+}
---
+++ API_Usuario/Migrations/20250824164049_AddMotivoDsligamentoIdDesligamento.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API_Usuario.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMotivoDsligamentoIdDesligamento : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "MotivoDesligamentoId", 
+                table: "desligamentos", 
+                type: "char(36)", 
+                nullable: false, 
+                collation: "ascii_general_ci"
+            );
+            migrationBuilder.AddForeignKey(
+                name: "FK_Desligamentos_MotivoDesligamentoId", 
+                table: "desligamentos",
+                column: "MotivoDesligamentoId", 
+                principalTable: "motivodesligamentos",
+                principalColumn: "Id"
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey("FK_Desligamentos_MotivoDesligamentoId", "desligamentos");
+            migrationBuilder.DropColumn("MotivoDesligamentoId", "desligamentos");
+        }
+    }
+}
