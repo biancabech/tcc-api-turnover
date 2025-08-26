eede078df764ee64e450b4718790ff3342b45576
Bianca Bech <biancapaulabech@gmail.com>
2025-08-26 00:03:00 -0300

    Criação de migration para alterar campo

---
+++ API_Usuario/Migrations/20250822114503_AddChaveUnicaCpfFuncionario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API_Usuario.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddChaveUnicaCpfFuncionario : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(name: "cpf", table: "funcionarios", type: "char(11)", nullable: false);
+            migrationBuilder.AddUniqueConstraint(name: "uniq_funcionario_cpf", table: "funcionarios", columns: ["cpf"]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropUniqueConstraint("uniq_funcionario_cpf", "funcionarios");
+            migrationBuilder.AlterColumn<string>(name: "cpf", table: "funcionarios", type: "longtext", nullable: false);
+        }
+    }
+}
