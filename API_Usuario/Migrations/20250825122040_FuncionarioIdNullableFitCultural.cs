a8aacc9759455550448b543abadf6b4c7a7c5b47
Bianca Bech <biancapaulabech@gmail.com>
2025-08-26 00:08:36 -0300

    Adicionado migration para modificar campo no fit cultural

---
+++ API_Usuario/Migrations/20250825122040_FuncionarioIdNullableFitCultural.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API_Usuario.Migrations
+{
+    /// <inheritdoc />
+    public partial class FuncionarioIdNullableFitCultural : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "FuncionarioId",
+                table: "fitculturals",
+                nullable: true,
+                type: "char(36)",
+                collation: "ascii_general_ci"
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "FuncionarioId",
+                table: "fitculturals",
+                nullable: false,
+                type: "char(36)",
+                collation: "ascii_general_ci"
+            );
+        }
+    }
+}
