5b51102fb5b7d9565d17869569a5cdc262c4ca1c
Bianca Bech <biancapaulabech@gmail.com>
2025-09-05 16:30:44 -0300

    Criado o serviço do IaIndica

---
+++ API_Usuario/Controllers/IaIndicaController.cs
@@ -0,0 +1,23 @@
+﻿using API_Usuario.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Usuario.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IaIndicaController : ControllerBase
+    {
+        public readonly IaIndicaServices _services;
+
+        public IaIndicaController(IaIndicaServices iaIndicaServices)
+        {
+            _services = iaIndicaServices;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok(new { resultado = _services.Analisar() });
+        }
+    }
+}
---
+++ API_Usuario/Services/IaIndicaServices.cs
@@ -0,0 +1,23 @@
+﻿using API_Usuario.Context;
+using API_Usuario.DTOs;
+using API_Usuario.Models;
+using Aspire.Hosting;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_Usuario.Services
+{
+    public class IaIndicaServices
+    {
+        public string Analisar() {
+            //var builder = DistributedApplication.CreateBuilder(args);
+
+            //var ollama = builder.AddOllama("ollama");
+
+            //var deepscaler = ollama.AddModel("deepscaler:1.5b");
+
+
+
+            return "Super relatório gerado com sucesso!";
+        }
+    }
+}
